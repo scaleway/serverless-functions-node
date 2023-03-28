48298d831372c52029c401d023189b23ac809d4f
Emilie BOUIN <ebouin@scaleway.com>
2023-03-28 15:19:22 +0200

    feat: add request, context and handler types

---
+++ framework/types/types.ts
@@ -0,0 +1,48 @@
+type RequestContext = {
+    accountId: string;
+    resourceId: string;
+    stage: string;
+    requestId: string;
+    resourcePath: string;
+    authorizer: Record<string, unknown> | null;
+    httpMethod: string;
+    apiId: string;
+}
+
+type Event = {
+    resource: string;
+    path: string;
+    httpMethod: string;
+    headers: Record<string, unknown> | null;
+    multiValueHeaders: Record<string, string[]> | null;
+    queryStringParameters: Record<string, string> | null;
+    multiValueQueryStringParameters: Record<string, string[]> | null;
+    pathParameters: null;
+    stageVariables: Record<string, string>;
+    requestContext: RequestContext;
+    body: string;
+    isBase64Encoded?: boolean;
+}
+
+type Context = {
+    memoryLimitInMb: number;
+    functionName: string;
+    functionVersion: string;
+}
+
+type Callback = {
+    (error: Error | undefined, result: unknown): void
+}
+
+type ResponseRecord = {
+    body?: string | Record<string, unknown>;
+    headers?: Record<string, string>;
+    statusCode?: number;
+    isBase64Encoded?: boolean;
+}
+
+export type Handler = (
+    event: Event,
+    context: Context,
+    callback: Callback
+) => string | ResponseRecord | Promise<string> | Promise<ResponseRecord>
