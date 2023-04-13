69c1092c9728523a77a2c93b2a585ff496e6be6e
Emilie BOUIN <48752456+Bemilie@users.noreply.github.com>
2023-04-13 15:00:48 +0200

    feat: add gha to release npm package when publishing new release on gh (#6)
    * feat: add gha to release npm package when publishing new release on gh
    
    * fix: lint

--- examples/with_event_components/handler.js
+++ examples/with_event_components/handler.js
@@ -19,7 +19,7 @@ export function handle(event, context, callback) {
 /* Module was not imported but called directly, so we can test locally.
 This will not be executed on Scaleway Functions */
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
-  import("scaleway-functions-node").then(scw_fnc_node => {
+  import("@scaleway/serverless-functions").then(scw_fnc_node => {
     scw_fnc_node.serveHandler(handle, 8080);
   });
 }
