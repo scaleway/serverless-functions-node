d9e3e5cbba90721ac117241d4e43669e78c4ca14
Simon Shillaker <sshillaker@scaleway.com>
2023-10-31 15:59:22 +0100

    Update everything

--- examples/with_http_services/handler.js
+++ examples/with_http_services/handler.js
@@ -34,53 +34,6 @@ export async function handle(event, context) {
   return response;
 }
 
-// POST request sample
-/*export async function handle (event, context) {
-    let dataString = "";
-
-    const postData = JSON.stringify({
-        name: "John Doe",
-        email: "jdoe@scaleway.com",
-    });
-
-    const options = {
-        hostname: "postman-echo.com",
-        path: "/post",
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-
-    const response = await new Promise((resolve, reject) => {
-        const req = https.request(options, (res) => {
-            res.on("data", (chunk) => {
-                dataString += chunk;
-            });
-
-            res.on("end", () => {
-                resolve({
-                    statusCode: 200,
-                    headers: { "Content-Type": ["application/json"] },
-                    body: JSON.stringify(JSON.parse(dataString), null, 4),
-                });
-            });
-
-            res.on("error", (e) => {
-                reject({
-                    statusCode: 500,
-                    body: "Something went wrong!",
-                });
-            });
-        });
-        // write data to request body
-        req.write(postData);
-        req.end();
-    });
-
-    return response;
-}*/
-
 /* Module was not imported but called directly, so we can test locally.
 This will not be executed on Scaleway Functions */
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
--- framework/local/index.ts
+++ framework/local/index.ts
@@ -11,12 +11,12 @@ export function serveHandler(handler: Handler, port = 8080) {
   server.addContentTypeParser(
     "application/x-www-form-urlencoded",
     { parseAs: "string" },
-    server.getDefaultJsonParser("ignore", "ignore"),
+    server.defaultTextParser,
   );
 
   server.addContentTypeParser(
     "multipart/form-data",
-    function (request, payload, done) {
+    function(request, payload, done) {
       let body = "";
       payload.on("data", d => {
         body += d;
@@ -27,8 +27,14 @@ export function serveHandler(handler: Handler, port = 8080) {
     },
   );
 
+  server.addContentTypeParser(
+    "application/json",
+    { parseAs: "string" },
+    server.defaultTextParser
+  )
+
   // Emulate core preprocess
-  server.addHook("preValidation", function (request, reply, done) {
+  server.addHook("preValidation", function(request, reply, done) {
     // Emulate the CoreRT guard
     if (isRejectedRequest(request)) {
       console.error(
@@ -41,7 +47,7 @@ export function serveHandler(handler: Handler, port = 8080) {
     done();
   });
 
-  server.addHook("onRequest", function (request, reply, done) {
+  server.addHook("onRequest", function(request, reply, done) {
     // Those headers are added for convenience, but will be overwritten if set in the handler
     reply.header("Access-Control-Allow-Origin", "*");
     reply.header("Access-Control-Allow-Headers", "Content-Type");
