6f366502448ae0f887cd8438d593e0c3e3dc5166
Simon Shillaker <sshillaker@scaleway.com>
2023-11-06 14:27:55 +0100

    Resurrect POST example

--- examples/with_http_get/handler.js
+++ examples/with_http_get/handler.js
@@ -34,8 +34,7 @@ export async function handle(event, context) {
   return response;
 }
 
-/* Module was not imported but called directly, so we can test locally.
-This will not be executed on Scaleway Functions */
+/* This will not be executed on Scaleway Functions */
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
   import("@scaleway/serverless-functions").then(scw_fnc_node => {
     scw_fnc_node.serveHandler(handle, 8080);
