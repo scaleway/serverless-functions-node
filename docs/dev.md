04920c76900beea612401c957627a439e1524487
Simon Shillaker <sshillaker@scaleway.com>
2023-11-06 15:14:38 +0100

    Fix formatting

--- docs/dev.md
+++ docs/dev.md
@@ -4,9 +4,40 @@ To test your local version of this package, you can use [`npm link`](https://doc
 
 You can do this quickly from the `examples` directory with:
 
-```
+```sh
 cd examples
 npm link ..
 
 node with_object/handler.js
+```
+
+## Multiple versions
+
+The tests run on multiple versions of Node. To manage multiple Node versions locally, you can use [`nvm`](https://github.com/nvm-sh/nvm).
+
+Once installed, you can install versions with:
+
+```sh
+# Install versions
+nvm install 16
+nvm install 18
+
+# Check
+nvm use 16
+npm i
+which node
+
+nvm use 18
+npm i
+which node
+```
+
+From there, you can run tests with different versions, e.g.
+
+```sh
+nvm use 16
+npm run format
+
+nvm use 18
+npm run format
 ```
--- examples/with_http_post/handler.js
+++ examples/with_http_post/handler.js
@@ -2,51 +2,51 @@ import * as https from "https";
 import { pathToFileURL } from "url";
 
 // POST request sample
-export async function handle (event, context) {
-    let dataString = "";
+export async function handle(event, context) {
+  let dataString = "";
 
-    const postData = JSON.stringify({
-        name: "John Doe",
-        email: "jdoe@scaleway.com",
-    });
+  const postData = JSON.stringify({
+    name: "John Doe",
+    email: "jdoe@scaleway.com",
+  });
 
-    const options = {
-        hostname: "postman-echo.com",
-        path: "/post",
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
+  const options = {
+    hostname: "postman-echo.com",
+    path: "/post",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const response = await new Promise((resolve, reject) => {
-        const req = https.request(options, (res) => {
-            res.on("data", (chunk) => {
-                dataString += chunk;
-            });
+  const response = await new Promise((resolve, reject) => {
+    const req = https.request(options, res => {
+      res.on("data", chunk => {
+        dataString += chunk;
+      });
 
-            res.on("end", () => {
-                resolve({
-                    statusCode: 200,
-                    headers: { "Content-Type": ["application/json"] },
-                    body: JSON.stringify(JSON.parse(dataString), null, 4),
-                });
-            });
-
-            res.on("error", (e) => {
-                reject({
-                    statusCode: 500,
-                    body: "Something went wrong!",
-                });
-            });
+      res.on("end", () => {
+        resolve({
+          statusCode: 200,
+          headers: { "Content-Type": ["application/json"] },
+          body: JSON.stringify(JSON.parse(dataString), null, 4),
         });
+      });
 
-        // Write data to request body
-        req.write(postData);
-        req.end();
+      res.on("error", e => {
+        reject({
+          statusCode: 500,
+          body: "Something went wrong!",
+        });
+      });
     });
 
-    return response;
+    // Write data to request body
+    req.write(postData);
+    req.end();
+  });
+
+  return response;
 }
 
 /* This will not be executed on Scaleway Functions */
